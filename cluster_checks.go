1b97514d3c8d115ba40d68a3fb5c0321be8009a9
Kristoffer Ahl <git@77dynamite.com>
2018-10-08 08:39:38 +0200

    - Fixed issue with cluster consensus check.

--- cluster_checks.go
+++ cluster_checks.go
@@ -9,18 +9,28 @@ func (cs *checkSet) doClusterConsensusChecks(nodeGossip []*gossipResponse) {
 	for _, ng := range nodeGossip {
 		for _, n := range ng.Members {
 			if n.IsAliveMaster() {
-				masters = append(masters, ng.ServerIP)
+				masters = append(masters, n.InstanceID)
 			}
 		}
 	}
 
 	masterCheck := cs.createCheck("master_consensus")
+	masterCheck.Data = distinct(masters)
 	if !all(masters, equal) || len(masters) != len(nodeGossip) {
 		masterCheck.fail("Node have different masters!")
-		masterCheck.Data = masters
-	} else {
-		masterCheck.Data = masters
 	}
+}
+
+func distinct(a []string) []string {
+	uniques := make([]string, 0)
+	uniques = append(uniques, a[0])
+
+	for i := 1; i < len(a); i++ {
+		if !equal(a[i], a[0]) {
+			uniques = append(uniques, a[i])
+		}
+	}
+	return uniques
 }
 
 func all(a []string, f func(a string, b string) bool) bool {
