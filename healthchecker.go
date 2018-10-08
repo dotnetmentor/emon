dbe49060af5537f78681fad3d685d73603de80bc
Kristoffer Ahl <git@77dynamite.com>
2018-10-08 08:10:58 +0200

    - Moved performance monitor to new file.

--- healthchecker.go
+++ healthchecker.go
@@ -118,32 +118,3 @@ func getNodes(endpoint string) []string {
 
 	return nodes
 }
-
-type perfMon struct {
-	name    string
-	results map[string]time.Duration
-}
-
-func (pm *perfMon) track(start time.Time, name string) {
-	elapsed := time.Since(start)
-	pm.results[name] = elapsed
-}
-
-func (pm *perfMon) getCheckSet() *checkSet {
-	cs := createCheckSet(pm.name, "emon")
-	check := cs.createCheck("slow_checks")
-	slow := make(map[string]int)
-
-	for k, ns := range pm.results {
-		if ns > config.EmonSlowCheckThreshold {
-			slow[k] = int(ns / time.Millisecond)
-		}
-	}
-
-	check.Data = slow
-	if len(slow) > 0 {
-		check.warn(fmt.Sprintf("One or more checks are performing badly (over %v)", config.EmonSlowCheckThreshold))
-	}
-
-	return cs
-}
